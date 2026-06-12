a51dafe99a5ff121b17ee3f34847c670667468f8
hesselsyves <yves.hessels@acerta.be>
2026-06-12 09:05:31 +0200

    Update mock_cities.py

--- mock_cities.py
+++ mock_cities.py
@@ -1,15 +1,21 @@
 from mitmproxy import http
 import json
 import os
-
-TARGET_URL = "https://t-api.acerta.be/stratencomponent/address/cities"
-
-
+dir_path = os.path.dirname(os.path.abspath(__file__))
+MOCK_RESPONSES = {
+    "https://t-api.acerta.be/stratencomponent/address/cities": json.load(
+        open(os.path.join(dir_path, "cities_data.json"))
+    ),
+    "https://t-api.acerta.be/stratencomponent/address/street/search": {
+        "streetNames": [
+            {"streetName": "Bondgenotenlaan", "streetCode": "12345"}
+        ]
+    },
+}
 def request(flow: http.HTTPFlow):
-    if flow.request.pretty_url.startswith(TARGET_URL):
-        dir_path = os.path.dirname(os.path.abspath(__file__))
-        with open(os.path.join(dir_path, "cities_data.json")) as f:
-            data = json.load(f)
-        flow.response = http.Response.make(
-            200, json.dumps(data).encode("utf-8"), {"Content-Type": "application/json"}
-        )
+    for url_prefix, data in MOCK_RESPONSES.items():
+        if flow.request.pretty_url.startswith(url_prefix):
+            flow.response = http.Response.make(
+                200, json.dumps(data).encode("utf-8"), {"Content-Type": "application/json"}
+            )
+            return
